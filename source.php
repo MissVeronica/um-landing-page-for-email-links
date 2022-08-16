06b4f9e3e4217958e86a63e8e8530114a54c8e89
MissVeronica <veronica@tranza.se>
2022-08-16 15:44:07 +0200

    Update source.php

--- source.php
+++ source.php
@@ -2,8 +2,9 @@
 
 // https://github.com/MissVeronica/um-landing-page-for-email-links
 //
-// Ultimate Member Landing Page for Email Links - Version 2 
-// Date: 2021-12-19
+// Ultimate Member Landing Page for Email Links - Version 3 
+// Supporting both UM upto 2.4.2 and UM 2.5.0 and afterwards
+// Date: 2022-08-16
 
 
 add_filter( 'wp_mail', 'my_wp_mail_filter', 10, 1 );
@@ -41,6 +42,7 @@ function um_landing_page() {
                 $user_id = $query[0]['user_id'];                
                 um_fetch_user( $user_id );
                 if( um_user( 'user_login') != sanitize_text_field( $_REQUEST['login'] )) $user_id = '';
+                elseif( is_user_logged_in() && absint( $user_id ) !== get_current_user_id()) $user_id = ''; // NEW 2022-01-14
 
             } else $user_id = '';
 
@@ -71,13 +73,22 @@ function um_landing_page() {
         
         if( $_REQUEST['myact'] == 'landing_for_password' ) {
             $reset_page = esc_url( str_replace( get_bloginfo( 'url' ), '', get_permalink( UM()->config()->permalinks['password-reset'] )));
+            
+            if( isset( $_REQUEST['login'] )) {      // UM 2.5.0 and afterwards
+                $value = $_REQUEST['login'];
+                $id = 'login';
+            }
+            if( isset( $_REQUEST['user_id'] )) {    // UM until 2.4.2
+                $value = $_REQUEST['user_id'];
+                $id = 'user_id';
+            }
 ?>
             <h4>Please click this button to reset your password.</h4>
                 <div>
                     <form id="account-password-form" action="<?php echo $reset_page;?>" method="get">
                         <input type="hidden" id="act" name="act" value="reset_password">
                         <input type="hidden" name="hash" value="<?php echo esc_html( $_REQUEST['hash'] );?>">
-                        <input type="hidden" id="user_id" name="user_id" value="<?php echo esc_html( $_REQUEST['user_id'] ); ?>" />
+                        <input type="hidden" id="<?php echo $id;?>" name="<?php echo $id;?>" value="<?php echo esc_html( $value ); ?>" />
                         <p><button data-action='submit'><?php echo 'Reset password now'; ?></button></p>
                     </form>
                 </div>
